b1f42e9aaffa5b8156368b70e0dc8e7f20925728
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-05-22 02:26:51 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
+import Navbar from "./Components/Shared/Navbar";
 
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold">
-        heyyyyy, this is a manufacturing website
-      </h1>
+      <Navbar />
     </div>
   );
 }
