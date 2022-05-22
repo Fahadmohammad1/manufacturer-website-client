70dfe876bc8a0ae5516c9a15768a938ad7bbd682
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-05-22 18:12:27 +0600

    navbar changed

--- src/Components/Shared/Navbar.js
+++ src/Components/Shared/Navbar.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../../Assets/images/logo.png";
 
 const Navbar = () => {
   return (
     <div class="navbar bg-base-100">
       <div class="navbar-start">
         <div class="dropdown">
-          <label tabindex="0" class="btn btn-ghost btn-circle">
+          <label tabindex="0" class="btn btn-ghost lg:hidden">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               class="h-5 w-5"
@@ -18,7 +19,7 @@ const Navbar = () => {
                 stroke-linecap="round"
                 stroke-linejoin="round"
                 stroke-width="2"
-                d="M4 6h16M4 12h16M4 18h7"
+                d="M4 6h16M4 12h8m-8 6h16"
               />
             </svg>
           </label>
@@ -27,58 +28,95 @@ const Navbar = () => {
             class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
           >
             <li>
-              <Link to="/">Homepage</Link>
+              <a>Item 1</a>
             </li>
             <li>
-              <Link to="/">Portfolio</Link>
+              <a>Item 2</a>
             </li>
             <li>
-              <Link to="/">About</Link>
+              <a>Item 3</a>
+            </li>
+          </ul>
+          {/* <div
+            class=" menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            tabIndex="0"
+          >
+            <a class="btn">Login</a>
+            <div class="dropdown dropdown-end">
+              <label tabindex="1" class="btn btn-ghost btn-circle avatar">
+                <div class="w-10 rounded-full">
+                  <img src="https://api.lorem.space/image/face?hash=33791" />
+                </div>
+              </label>
+              <ul
+                tabindex="1"
+                class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+              >
+                <li>
+                  <a class="justify-between">
+                    Profile
+                    <span class="badge">New</span>
+                  </a>
+                </li>
+                <li>
+                  <a>Settings</a>
+                </li>
+                <li>
+                  <a>Logout</a>
+                </li>
+              </ul>
+            </div>
+          </div> */}
+        </div>
+        <div class="btn btn-ghost normal-case text-xl">
+          <div className="w-12">
+            <img src={logo} alt="" />
+          </div>
+          <a href="*" className="uppercase">
+            Mars Technology
+          </a>
+        </div>
+      </div>
+      <div class="navbar-center hidden lg:flex">
+        <ul class="menu menu-horizontal p-0">
+          <li>
+            <a>Item 1</a>
+          </li>
+          <li>
+            <a>Item 2</a>
+          </li>
+
+          <li>
+            <a>Item 3</a>
+          </li>
+        </ul>
+      </div>
+      <div class="navbar-end hidden lg:flex">
+        <a class="btn">Login</a>
+        <div class="dropdown dropdown-end">
+          <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+            <div class="w-10 rounded-full">
+              <img src="https://api.lorem.space/image/face?hash=33791" />
+            </div>
+          </label>
+          <ul
+            tabindex="0"
+            class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a class="justify-between">
+                Profile
+                <span class="badge">New</span>
+              </a>
+            </li>
+            <li>
+              <a>Settings</a>
+            </li>
+            <li>
+              <a>Logout</a>
             </li>
           </ul>
         </div>
-      </div>
-      <div class="navbar-center">
-        <Link to="/" class="btn btn-ghost normal-case text-xl">
-          Mars Technology
-        </Link>
-      </div>
-      <div class="navbar-end">
-        <button class="btn btn-ghost btn-circle">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            />
-          </svg>
-        </button>
-        <button class="btn btn-ghost btn-circle">
-          <div class="indicator">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-              />
-            </svg>
-            <span class="badge badge-xs badge-primary indicator-item"></span>
-          </div>
-        </button>
       </div>
     </div>
   );
