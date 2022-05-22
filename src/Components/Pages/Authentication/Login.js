44b48de0a37226260e2e2328242ade906f37a2eb
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-05-23 03:31:33 +0600

    error showed

--- src/Components/Pages/Authentication/Login.js
+++ src/Components/Pages/Authentication/Login.js
@@ -1,24 +1,24 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
 // import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [user] = useAuthState(auth);
   const {
     register,
     formState: { errors },
     handleSubmit,
   } = useForm();
 
-  console.log(errors);
   const onSubmit = (e) => {};
   return (
-    <div className="h-screen flex">
+    <div className="h-screen flex container mx-auto">
       <div className="flex w-1/2 bg-gradient-to-tr from-blue-800 to-purple-700 i justify-around items-center">
         <div>
           <h1 className="text-white font-bold text-4xl font-sans">GoFinance</h1>
-          <p className="text-white mt-1">
-            The most popular peer to peer lending at SEA
-          </p>
+          <p className="text-white mt-1">The most popular brand in Asia</p>
           <button
             type="submit"
             className="block w-28 bg-white text-indigo-800 mt-4 py-2 rounded-2xl font-bold mb-2"
