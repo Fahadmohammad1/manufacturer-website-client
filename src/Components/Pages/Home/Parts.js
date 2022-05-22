e9489ce7b6d001678bbe93ae24782bd04cc67800
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-05-22 14:02:06 +0600

    single part showed

--- src/Components/Pages/Home/Parts.js
+++ src/Components/Pages/Home/Parts.js
@@ -1,12 +1,17 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 import { useQuery } from "react-query";
+import Part from "./Part";
 
 const Parts = () => {
   //   const [parts, setParts] = useState([]);
 
-  const { data: parts } = useQuery("parts", () =>
+  const { data: parts, isLoading } = useQuery("parts", () =>
     fetch("data.json").then((res) => res.json())
   );
+
+  if (isLoading) {
+    return <p>please wait</p>;
+  }
   console.log(parts);
 
   return (
@@ -14,6 +19,11 @@ const Parts = () => {
       <h2 className="text-2xl uppercase font-bold text-center">
         parts we provide
       </h2>
+      <div className="grid lg:grid-cols-3 gap-y-10">
+        {parts.map((part) => (
+          <Part part={part} key={part.id}></Part>
+        ))}
+      </div>
     </div>
   );
 };
