c2f57dfe6c6ae0e12d2a2c4cca31c780f227f210
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-05-22 14:16:22 +0600

    part showed in card

--- src/Components/Pages/Home/Part.js
+++ src/Components/Pages/Home/Part.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Part = ({ part }) => {
-  const { image, price, id, name } = part;
+  const { image, price, name } = part;
   return (
     <div>
       <div class="flex flex-col items-center justify-center max-w-sm mx-auto">
@@ -18,9 +18,11 @@ const Part = ({ part }) => {
           </h3>
 
           <div class="flex items-center justify-between px-3 py-2 bg-gray-200 dark:bg-gray-700">
-            <span class="font-bold text-gray-800 dark:text-gray-200">$129</span>
+            <span class="font-bold text-gray-800 dark:text-gray-200">
+              ${price}
+            </span>
             <button class="px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-200 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">
-              Add to cart
+              purchase now
             </button>
           </div>
         </div>
