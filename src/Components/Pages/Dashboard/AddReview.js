d8fe2ec3597ce37c7e48334323bde90da88337b9
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-05-25 21:55:40 +0600

    table added

--- src/Components/Pages/Dashboard/AddReview.js
+++ src/Components/Pages/Dashboard/AddReview.js
@@ -1,9 +1,34 @@
+import axios from "axios";
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
+import Loading from "../../Shared/Loading";
 
 const AddReview = () => {
+  const [user, loading] = useAuthState(auth);
+  const { data } = axios
+    .get(`http://localhost:5000/myOrder/${user.email}`)
+    .then((res) => res.json());
+
+  if (loading) {
+    return <Loading />;
+  }
+  console.log(data);
   return (
     <div>
-      <h1>this is add review outlet</h1>
+      <div class="overflow-x-auto">
+        <table class="table table-zebra w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Quantity</th>
+            </tr>
+          </thead>
+          <tbody></tbody>
+        </table>
+      </div>
     </div>
   );
 };
